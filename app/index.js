c309f2faacbc7470597c3c39a28449feac41db4f
U-Legendary-PC\Ian <Ian@Legendary-PC.(none)>
2013-08-18 13:55:36 -0400

    Initial Generator. Not sure if it builds yet.

---
+++ app/index.js
@@ -0,0 +1,228 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var spawn = require('child_process').spawn;
+var yeoman = require('yeoman-generator');
+
+// The "magic" that starts the module and runs all the prototype functions in the order they are written
+var BreiAppGenerator = module.exports = function BreiAppgenerator(args, options, config) {
+  yeoman.generators.Base.apply(this, arguments);
+
+  // Mocha is fine
+
+  // setup the test-framework property, Gruntfile template will need this
+  this.testFramework = options['test-framework'] || 'mocha';
+
+  // for hooks to resolve on mocha by default
+  if (!options['test-framework']) {
+    options['test-framework'] = 'mocha';
+  }
+
+  // resolved to mocha by default (could be switched to jasmine for instance)
+  this.hookFor('test-framework', { as: 'app' });
+
+  // Read in the index file so we can append stuff to it later
+  this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+
+  // Install bower stuff
+  this.on('end', function () {
+    this.installDependencies({ skipInstall: options['skip-install'] });
+  });
+
+  // Read in package info
+  this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(BreiAppGenerator, yeoman.generators.Base);
+
+BreiAppGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // welcome message
+  console.log(this.yeoman);
+  console.log('Out of the box I include HTML5 Boilerplate, jQuery (1.x) and Modernizr.');
+
+  var prompts = [{
+    type: 'checkbox',
+    name: 'features',
+    message: 'What more would you like?',
+    choices: [{
+    //   name: 'Twitter Bootstrap',
+    //   value: 'useBootstrap',
+    //   checked: false
+    // }, {
+      name: 'Use SASS/Compass',
+      value: 'includeSass',
+      checked: false
+    }, {
+    //   name: 'RequireJS',
+    //   value: 'includeRequireJS',
+    //   checked: false
+    // }, {
+    // Uncomment this when I figure out the best way to add Angular dynamically
+    //   name: 'AngularJS',
+    //   value: 'includeAngularJS',
+    //   checked: false
+    // }, {
+      name: 'Autoprefixer for your CSS',
+      value: 'autoprefixer',
+      checked: true
+    },
+    {
+      type: 'input',
+      name: 'Deploy directory (relative to current path)',
+      value: 'deployDirectory',
+      default: "../../deploy"
+    }]
+  }];
+
+  this.prompt(prompts, function (answers) {
+    var features = answers.features;
+
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    // this.useBootstrap = features.indexOf('useBootstrap') !== -1;
+    this.includeSass = features.indexOf('includeSass') !== -1;
+    // this.includeAngularJS = features.indexOf('includeAngularJS') !== -1;
+    // this.includeRequireJS = features.indexOf('includeRequireJS') !== -1;
+    this.autoprefixer = features.indexOf('autoprefixer') !== -1;
+
+    cb();
+  }.bind(this));
+};
+
+BreiAppGenerator.prototype.gruntfile = function gruntfile() {
+  this.template('Gruntfile.js');
+};
+
+BreiAppGenerator.prototype.packageJSON = function packageJSON() {
+  this.template('_package.json', 'package.json');
+};
+
+// We use SVN
+//
+// BreiAppGenerator.prototype.git = function git() {
+//   this.copy('gitignore', '.gitignore');
+//   this.copy('gitattributes', '.gitattributes');
+// };
+
+BreiAppGenerator.prototype.bower = function bower() {
+  this.copy('bowerrc', '.bowerrc');
+  this.copy('_bower.json', 'bower.json');
+};
+
+BreiAppGenerator.prototype.jshint = function jshint() {
+  this.copy('jshintrc', '.jshintrc');
+};
+
+// We don't care about this
+//
+// BreiAppGenerator.prototype.editorConfig = function editorConfig() {
+//   this.copy('editorconfig', '.editorconfig');
+// };
+//
+// or this
+//
+// BreiAppGenerator.prototype.h5bp = function h5bp() {
+//   this.copy('favicon.ico', 'app/favicon.ico');
+//   this.copy('404.html', 'app/404.html');
+//   this.copy('robots.txt', 'app/robots.txt');
+//   this.copy('htaccess', 'app/.htaccess');
+// };
+
+// BreiAppGenerator.prototype.bootstrapImg = function bootstrapImg() {
+//   if (this.compassBootstrap) {
+//     this.copy('glyphicons-halflings.png', 'app/img/glyphicons-halflings.png');
+//     this.copy('glyphicons-halflings-white.png', 'app/img/glyphicons-halflings-white.png');
+//   }
+// };
+
+// BreiAppGenerator.prototype.bootstrapJs = function bootstrapJs() {
+//   // TODO: create a Bower component for this
+//   if (this.useBootstrap) {
+//     this.copy('bootstrap.js', 'app/js/vendor/bootstrap.js');
+//   }
+// };
+
+BreiAppGenerator.prototype.mainStylesheet = function mainStylesheet() {
+  if (this.includeSass) {
+    this.copy('main.scss', 'app/css/main.scss');
+  } else {
+    this.copy('main.css', 'app/css/main.css');
+  }
+};
+
+BreiAppGenerator.prototype.writeIndex = function writeIndex() {
+  // prepare default content text
+  var defaults = ['HTML5 Boilerplate'];
+  var contentText = [
+    '        <div class="container">',
+  ];
+
+  // if (!this.includeRequireJS) {
+  //   this.indexFile = this.appendScripts(this.indexFile, 'js/main.js', [
+  //     'bower_components/jquery/jquery.js',
+  //     'js/main.js'
+  //   ]);
+
+  //   this.indexFile = this.appendFiles({
+  //     html: this.indexFile,
+  //     fileType: 'js',
+  //     optimizedPath: 'js/coffee.js',
+  //     sourceFileList: ['js/hello.js'],
+  //     searchPath: '.tmp'
+  //   });
+  // }
+
+  // if (this.compassBootstrap) {
+  //   defaults.push('Twitter Bootstrap');
+  // }
+
+  // if (this.compassBootstrap && !this.includeRequireJS) {
+  //   // wire Twitter Bootstrap plugins
+  //   this.indexFile = this.appendScripts(this.indexFile, 'js/plugins.js', [
+  //     'bower_components/sass-bootstrap/js/bootstrap-affix.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-alert.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-dropdown.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-tooltip.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-modal.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-transition.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-button.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-popover.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-typeahead.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-carousel.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-scrollspy.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-collapse.js',
+  //     'bower_components/sass-bootstrap/js/bootstrap-tab.js'
+  //   ]);
+  // }
+
+  // if (this.includeRequireJS) {
+  //   defaults.push('RequireJS');
+  // }
+
+  // iterate over defaults and create content string
+  // defaults.forEach(function (el) {
+  //   contentText.push('                    <li>' + el  +'</li>');
+  // });
+
+  contentText = contentText.concat([
+    '        </div>',
+    ''
+  ]);
+
+  // append the default content
+  this.indexFile = this.indexFile.replace('<body>', '<body>\n' + contentText.join('\n'));
+};
+
+BreiAppGenerator.prototype.app = function app() {
+  this.mkdir('app');
+  this.mkdir('app/js');
+  this.mkdir('app/css');
+  this.mkdir('app/css/i');
+  this.mkdir('app/img');
+  this.write('app/index.html', this.indexFile);
+
+  this.write('app/js/main.js', '// Main JavaScript file');
+  
+};
---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,361 @@
+// Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // configurable paths
+    var yeomanConfig = {
+        app: 'app',
+        dist: 'dist',
+        deploy: '<%= deployDirectory %>'
+    };
+
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        watch: {
+        // <% if (includeSass) { %>
+        //     compass: {
+        //         files: ['<%%= yeoman.app %>/css/{,*/}*.{scss,sass}'],
+        //         tasks: ['compass:server'<% if (autoprefixer) { %>, 'autoprefixer'<% } %>]
+        //     },<% } %>
+            styles: {
+                files: ['<%%= yeoman.app %>/css/{,*/}*.css'],
+                tasks: ['copy:styles'<% if (autoprefixer) { %>, 'autoprefixer'<% } %>]
+            },
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
+                files: [
+                    '<%%= yeoman.app %>/*.html',
+                    '.tmp/css/{,*/}*.css',
+                    '{.tmp,<%%= yeoman.app %>}/js/{,*/}*.js',
+                    '<%%= yeoman.app %>/img/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, yeomanConfig.app)
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, 'test')
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, yeomanConfig.dist)
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%%= connect.options.port %>'
+            }
+        },
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%%= yeoman.dist %>/*',
+                        '!<%%= yeoman.dist %>/.git*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '<%%= yeoman.app %>/js/{,*/}*.js',
+                '!<%%= yeoman.app %>/js/vendor/*',
+                'test/spec/{,*/}*.js'
+            ]
+        },<% if (testFramework === 'mocha') { %>
+        mocha: {
+            all: {
+                options: {
+                    run: true,
+                    urls: ['http://localhost:<%%= connect.options.port %>/index.html']
+                }
+            }
+        },<% } else if (testFramework === 'jasmine') { %>
+        jasmine: {
+            all: {
+                options: {
+                    specs: 'test/spec/{,*/}*.js'
+                }
+            }
+        },<% } %><% if (includeSass) { %>
+        compass: {
+            options: {
+                sassDir: '<%%= yeoman.app %>/css',
+                cssDir: '.tmp/css',
+                generatedImagesDir: '.tmp/img/generated',
+                imagesDir: '<%%= yeoman.app %>/img',
+                javascriptsDir: '<%%= yeoman.app %>/js',
+                fontsDir: '<%%= yeoman.app %>/css/fonts',
+                importPath: '<%%= yeoman.app %>/bower_components',
+                httpImagesPath: '/img',
+                httpGeneratedImagesPath: '/img/generated',
+                httpFontsPath: '/css/fonts',
+                relativeAssets: false
+            },
+            dist: {
+                options: {
+                    generatedImagesDir: '<%%= yeoman.dist %>/img/generated'
+                }
+            },
+            server: {
+                options: {
+                    debugInfo: true
+                }
+            }
+        },<% } %><% if (autoprefixer) { %>
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions', 'ie 8', 'ie 9']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/css/',
+                    src: '{,*/}*.css',
+                    dest: '.tmp/css/'
+                }]
+            }
+        },<% } %>
+        // not used since Uglify task does concat,
+        // but still available if needed
+        /*concat: {
+            dist: {}
+        },*/
+        // not enabled since usemin task does concat and uglify
+        // check index.html to edit your build targets
+        // enable this task if you prefer defining your build targets here
+        /*uglify: {
+            dist: {}
+        },*/
+        rev: {
+            dist: {
+                files: {
+                    src: [
+                        '<%%= yeoman.dist %>/js/{,*/}*.js',
+                        '<%%= yeoman.dist %>/css/{,*/}*.css',
+                        '<%%= yeoman.dist %>/img/{,*/}*.{png,jpg,jpeg,gif,webp}',
+                        '<%%= yeoman.dist %>/css/fonts/*'
+                    ]
+                }
+            }
+        },
+        useminPrepare: {
+            options: {
+                dest: '<%%= yeoman.dist %>'
+            },
+            html: '<%%= yeoman.app %>/index.html'
+        },
+        usemin: {
+            options: {
+                dirs: ['<%%= yeoman.dist %>']
+            },
+            html: ['<%%= yeoman.dist %>/{,*/}*.html'],
+            css: ['<%%= yeoman.dist %>/css/{,*/}*.css']
+        },
+        imagemin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.app %>/img',
+                    src: '{,*/}*.{png,jpg,jpeg}',
+                    dest: '<%%= yeoman.dist %>/img'
+                },
+                {
+                    expand: true,
+                    cwd: '<%%= yeoman.app %>/css/i',
+                    src: '{,*/}*.{png,jpg,jpeg,gif,webp}',
+                    dest: '<%%= yeoman.dist %>/css/i'
+                }]
+            }
+        },
+        svgmin: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.app %>/img',
+                    src: '{,*/}*.svg',
+                    dest: '<%%= yeoman.dist %>/img'
+                }]
+            }
+        },
+        cssmin: {
+            // This task is pre-configured if you do not wish to use Usemin
+            // blocks for your CSS. By default, the Usemin block from your
+            // `index.html` will take care of minification, e.g.
+            //
+            //     <!-- build:css({.tmp,app}) styles/main.css -->
+            //
+            // dist: {
+            //     files: {
+            //         '<%%= yeoman.dist %>/css/main.css': [
+            //             '.tmp/css/{,*/}*.css',
+            //             '<%%= yeoman.app %>/css/{,*/}*.css'
+            //         ]
+            //     }
+            // }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    /*removeCommentsFromCDATA: true,
+                    // https://github.com/yeoman/grunt-usemin/issues/44
+                    //collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeAttributeQuotes: true,
+                    removeRedundantAttributes: true,
+                    useShortDoctype: true,
+                    removeEmptyAttributes: true,
+                    removeOptionalTags: true*/
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%%= yeoman.app %>',
+                    src: '*.html',
+                    dest: '<%%= yeoman.dist %>'
+                }]
+            }
+        },
+        // Put files not handled in other tasks here
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%%= yeoman.app %>',
+                    dest: '<%%= yeoman.dist %>',
+                    src: [
+                        '*.{ico,png,txt}',
+                        '.htaccess',
+                        'images/{,*/}*.{webp,gif}',
+                        'styles/fonts/*'
+                    ]
+                }]
+            },
+            deploy: {                
+                expand: true,
+                dot: true,
+                cwd: '<%%= yeoman.dist %>',
+                dest: '<%%= yeoman.deploy %>',
+                src: [
+                    '**'
+                ]
+            }
+            styles: {
+                expand: true,
+                dot: true,
+                cwd: '<%%= yeoman.app %>/css',
+                dest: '.tmp/css/',
+                src: '**.css'
+            }
+        },
+        concurrent: {
+            server: [<% if (includeSass) { %>
+                'compass',<% } %>
+                'copy:styles'
+            ],
+            test: [
+                'copy:styles'
+            ],
+            dist: [<% if (includeSass) { %>
+                'compass',<% } %>
+                'copy:styles',
+                'imagemin',
+                'svgmin',
+                'htmlmin'
+            ]
+        }
+    });
+
+    grunt.registerTask('server', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            'concurrent:server',<% if (autoprefixer) { %>
+            'autoprefixer',<% } %>
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('test', [
+        'clean:server',
+        'concurrent:test',<% if (autoprefixer) { %>
+        'autoprefixer',<% } %>
+        'connect:test',<% if (testFramework === 'mocha') { %>
+        'mocha'<% } else if (testFramework === 'jasmine') { %>
+        'jasmine'<% } %>
+    ]);
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'useminPrepare',
+        'concurrent:dist',<% if (autoprefixer) { %>
+        'autoprefixer',<% } %>
+        'concat',
+        'cssmin',
+        'uglify',
+        'copy:dist',
+        'rev',
+        'usemin'
+    ]);
+
+    grunt.registerTask('default', [
+        'jshint',
+        'test',
+        'build'
+    ]);
+};
